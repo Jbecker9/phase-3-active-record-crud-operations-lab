3ee0af10f999609472d29d5a02333846ce7bbaae
Jared <jbeckersalax9@aol.com>
2022-05-20 13:35:32 -0400

    commit tree 0617d1b1783a65477c6edc63d9d8bec9cccb3197

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
+require "pry"
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.find_or_create_by(title: title)
+        # Movie.new(title: title)
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attr)
+        Movie.find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(new_value)
+        # binding.pry
+        self.update(new_value)
+    end
+
+    def self.update_all_titles(new_titles)
+        # binding.pry
+        Movie.update(:all, title: new_titles)
+    end
+
+    def self.delete_by_id(deleted_id)
+        Movie.find(deleted_id).destroy
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
